c03aa1baff29ed2bc68a6eaadc5023b667a6cf48
marin-sandra <merinsandraphilip@gmail.com>
2022-07-27 09:51:56 +0000

    adding temp changes to github

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,19 +10,21 @@ urlpatterns = [
     # name the URL
 
     # path for about view
+    path(route='about/', view=views.get_about, name='about'),
 
     # path for contact us view
-
+    path(route='contact/', view=views.get_contact, name='contact'),
     # path for registration
-
+    path('registration/', views.registration_request, name='registration'),
     # path for login
-
+    path(route='login/', view=views.login_request, name='login'),
     # path for logout
+    path(route='logout/', view=views.logout_request, name='logout'),
 
     path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
-
+    path('dealer/<str:dealer_name>/<int:dealer_id>/', views.get_dealer_details, name='dealer_details'),
     # path for add a review view
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
